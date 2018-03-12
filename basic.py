c43996e3754a58dcd00803766de15703c108ae1b
Matthew Farrellee <matt@redhat.com>
2018-03-12 14:25:31 -0400

    base64 decoder

---
+++ basic.py
@@ -0,0 +1,6 @@
+def main(args):
+    try:
+        decoded = args['__ow_body'].decode('base64').strip()
+        return {"body": decoded}
+    except:
+        return {"body": "Could not decode body from base64"}
