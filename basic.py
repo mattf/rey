a8a36dcdef23cee3581eaafa4af72451ddbbbb91
Matthew Farrellee <matt@redhat.com>
2018-03-12 15:08:38 -0400

    basic fake service

--- basic.py
+++ basic.py
@@ -1,6 +1,29 @@
+import random
+
+CATEGORIES = [
+    'backpack',
+    'bag',
+    'desk',
+    'furniture',
+    'table',
+    'accessories',
+    'handbag',
+    'purse',
+    'bottle',
+    'dish',
+    'food',
+    'meal',
+    'plate',
+    'shop',
+    'bowl',
+    'tabletop'
+]
+
 def main(args):
     try:
-        decoded = args['__ow_body'].decode('base64').strip()
-        return {"body": decoded}
+        return {"labels":
+                    map(lambda l: {'name': l,
+                                    'confidence': random.uniform(.5,1)},
+                        random.sample(CATEGORIES, 5))}
     except:
-        return {"body": "Could not decode body from base64"}
+        return {"error": "Some failure, sorry"}
